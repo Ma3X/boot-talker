5bf68a8e6a30221fbffa88ca7541235cb5eecbb3
root <root@prog.itex>
2014-05-06 17:05:09 +0400

    fixing test-1 run scripts and environments

--- toolchains/tests/test-1/a.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-./arm-none-eabi-as -meabi=5 ./a.S -o ./a.o
-./arm-none-eabi-ld ./a.o -T ./a.lds -o ./a.elf
-./arm-none-eabi-objcopy -O binary ./a.elf ./a.bin
-./arm-none-eabi-objdump ./a.elf --disassemble-all > ./a.dis
