56e194d4a97dc83b29d900df2ac78abf3d944837
Rus Selin <xpyct.am.83@gmail.com>
2014-08-25 22:08:09 +0400

    osmocomm loader succes compiled

--- 3rd-party/osmocom-bb/firmwares/mt62xx/manifest.c
+++ 3rd-party/osmocom-bb/firmwares/mt62xx/manifest.c
@@ -1,12 +1,6 @@
-
 #include "manifest.h"
 
-//const char *manifest_application = APPLICATION;
-//const char *manifest_revision = GIT_REVISION;
-//const char *manifest_board = BOARD;
-//const char *manifest_environment = ENVIRONMENT;
-
-const char *manifest_application = "app";
-const char *manifest_revision = "git-rev";
-const char *manifest_board = "brd";
-const char *manifest_environment = "env";
+const char *manifest_application = APPLICATION;
+const char *manifest_revision = GIT_REVISION;
+const char *manifest_board = BOARD;
+const char *manifest_environment = ENVIRONMENT;
